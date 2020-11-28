0ded2042d2a12571be3e093f202ed513162e2433
Iristabi <73836100+Iristabi@users.noreply.github.com>
2020-11-28 15:46:17 +0100

    Add files via upload

---
+++ solution-module4/speakHello.js
@@ -0,0 +1,12 @@
+
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+(function(window) {
+  var helloSpeaker = {};
+  helloSpeaker.speak = function(name) {
+    console.log(speakWord + " " + name);
+  }
+  var speakWord = "Hello";
+  window.helloSpeaker = helloSpeaker;
+  
+}(window));
